ab2641ed65fb0bedfca26dafef1f8a28a9158e07
jaugeni <jaugeni@gmail.com>
2017-01-12 10:56:37 -0500

    Alamofire instoled

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by YAUHENI IVANIUK on 1/11/17.
+//  Copyright © 2017 YauheniIvaniuk. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+   
+
+}
+
