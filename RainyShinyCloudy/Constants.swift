0703f880f47460167388c94eeadc434b025ad6a9
jaugeni <jaugeni@gmail.com>
2017-01-17 23:11:33 -0500

    small upd

--- RainyShinyCloudy/Constants.swift
+++ RainyShinyCloudy/Constants.swift
@@ -10,7 +10,9 @@ import Foundation
 
 typealias DownloadCompleate = () -> ()
 
-let currentWeatherURLBasic = "http://api.openweathermap.org/data/2.5/weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid="
+let apiId = "ef3d038f7b3f9178866f296d63e1158b"
 
-let forecastURLBasic = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid="
+let currentWeatherURLBasic = "http://api.openweathermap.org/data/2.5/weather?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&appid=\(apiId)"
+
+let forecastURLBasic = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid=\(apiId)"
 
